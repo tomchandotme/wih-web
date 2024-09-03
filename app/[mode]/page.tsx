a8161ad97ac4140fc1ba8fffff3fc3c19494848e
tomchan <tomchan00727@gmail.com>
2024-09-04 01:10:32 +0800

    fuck

--- app/[mode]/page.tsx
+++ app/[mode]/page.tsx
@@ -1,6 +1,5 @@
 import { redirect } from "next/navigation"
 import { List } from "./list"
-import { getMods } from "@/items/mods"
 
 export default function ListPage({ params }: { params: { mode: string } }) {
   if (params.mode !== "owned" && params.mode !== "wishlisted") {
